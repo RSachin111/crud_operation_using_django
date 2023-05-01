be918d850de6ddca422ec5100897852a4f702d6c
Rsachin111 <sachinrathor880@gmail.com>
2023-05-01 17:43:52 +0530

    Change directory of the project

--- app2/models.py
+++ app2/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Employees(models.Model):
-    name=models.CharField(max_length=20)
-    email=models.EmailField(max_length=20)
-    phone=models.IntegerField()
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=30)
+    phone=models.BigIntegerField()
     address=models.TextField()
 
     def __str__(self):
